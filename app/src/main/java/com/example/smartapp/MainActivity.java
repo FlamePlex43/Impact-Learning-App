fcf659bfe5bcd96eb5db2e31ce31a49d1b43914e
Nihar <patilnihar2001@gmail.com>
2023-09-29 16:55:48 -0500

    - Start button redirects to choose subject screen

--- app/src/main/java/com/example/smartapp/MainActivity.java
+++ app/src/main/java/com/example/smartapp/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.smartapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-import com.example.smartapp.ChooseSubjects;
-import com.example.smartapp.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button next = findViewById(R.id.button);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Button Clicked");
-                Intent chooseTopicIntent = new Intent(getApplicationContext(), ChooseSubjects.class);
-                startActivity(chooseTopicIntent);
-
-            }
-        });
+        Button startButton = this.findViewById(R.id.startButton);
+        startButton.setOnClickListener(v -> openChooseSubjects());
+    }
+    public void openChooseSubjects(){
+        Intent intent = new Intent(this, ChooseSubjects.class);
+        startActivity(intent);
     }
 }
